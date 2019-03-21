8e1218a41a2752efdc0baf37bb5c05319a78ddf2
Abdallah Nsir <abdallahnsir33@gmail.com>
2019-03-22 02:01:17 +0300

    Initial Commit

---
+++ FindHouseForMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindHouseForMe
+//
+//  Created by Abdullah Jacksi on 3/22/19.
+//  Copyright © 2019 Abdullah Jacksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
